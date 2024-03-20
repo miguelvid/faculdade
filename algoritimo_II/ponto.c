815ce05ed04d650a94820ac2c89b301a260ea3c9
Miguel Vidor <142550095+miguelvid@users.noreply.github.com>
2024-03-20 14:57:11 -0300

    Update ponto.c

--- algoritimo_II/ponto.c
+++ algoritimo_II/ponto.c
@@ -5,22 +5,17 @@ int main() {
     ponto p1, p2;
     float x, y;
 
-    // Solicitar valores para o Ponto 1
     printf("Coordenadas Ponto 1 (x y): ");
     scanf("%f %f", &x, &y);
 
-    // Inicializar o Ponto 1 utilizando as funções de atributos
     setx(&p1, x);
     sety(&p1, y);
 
-    // Solicitar valores para o Ponto 2
     printf("Coordenadas Ponto 2 (x y): ");
     scanf("%f %f", &x, &y);
 
-    // Inicializar o Ponto 2 utilizando a função de ponto
     setponto(&p2, x, y);
 
-    // Mostrar os valores dos dois pontos
     printf("\nPonto 1: (%.2f, %.2f)\n", getx(&p1), gety(&p1));
     printf("Ponto 2: (%.2f, %.2f)\n", getx(&p2), gety(&p2));
 
